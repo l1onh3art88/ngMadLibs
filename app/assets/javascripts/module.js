e93bb42445081050d8da1273d114039b208d95a1
l1on3hart88 <spenccheng@gmail.com>
2015-07-08 15:14:56 +0000

    adding a controller

---
+++ app/assets/javascripts/module.js
@@ -0,0 +1,12 @@
+angular.module('ngMadLibs',[]).controller('myCtrl', function($scope){
+  $scope.male_name ="Bob";
+  $scope.dirty_task="Stuff";
+  $scope.obnoxious_celebrity="Paris Hilton";
+  $scope.job_title="Basket Weaver";
+  $scope.celebrity="Ariana Grande";
+  $scope.huge_number="10000";
+  $scope.tedious_task="Knitting";
+  $scope.useless_skill="jumping over small distances";
+  $scope.adjective="hilarious";
+
+});
