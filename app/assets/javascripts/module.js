f74faee4bea86f4a8bd2b654d70c29f7d32fa8a2
l1on3hart88 <spenccheng@gmail.com>
2015-07-08 20:15:30 +0000

    mad libs refactor two part one

--- app/assets/javascripts/module.js
+++ app/assets/javascripts/module.js
@@ -5,10 +5,22 @@ var ngMadLibs =angular.module('ngMadLibs',['ngMessages']);
   $scope.obnoxious_celebrity="Paris Hilton";
   $scope.job_title="Basket Weaver";
   $scope.celebrity="Ariana Grande";
-  $scope.huge_number="10000";
+  $scope.huge_number=10000;
   $scope.tedious_task="Knitting";
   $scope.useless_skill="jumping over small distances";
   $scope.adjective="hilarious";
   $scope.madlibs = false;
-
+  $scope.submit = function(){
+    console.log($scope.madlibform);
+    if( $scope.madlibform.$valid ) {
+    console.log('The form is valid');
+    }
+    else {
+    $scope.madlibs=false;
+    console.log('The form is invalid');
+   }
+  };
+  $scope.reset = function(){
+    $scope.madlibform.$setValidity("required",false);
+  }
 });
